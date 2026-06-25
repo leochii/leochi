3b1b7bf1a9ef5d276667e059fa4097afe01e45e3
nimaadiidar <youremail@example.com>
2026-06-24 18:54:11 -0700

    fix about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-5xl font-serif mb-8">
+        About LEOCHI
+      </h1>
+
+      <p>
+        EST 2019 — Premium Streetwear inspired by Persian culture.
+      </p>
+    </main>
+  );
+}
