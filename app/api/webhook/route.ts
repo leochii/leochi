8a95f49796717231862107af3ad4d8b8a7ccf76e
nimaadiidar <youremail@example.com>
2026-06-29 12:25:17 -0700

    Update webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,20 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  return NextResponse.json({
-    received: true,
-  });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    console.log("Stripe Webhook:", body);
+
+    return NextResponse.json({
+      received: true,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Webhook failed" },
+      { status: 500 }
+    );
+  }
 }
