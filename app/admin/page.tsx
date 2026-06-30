4da9b1e4538520add6d66ffc9d28f08979eb645b
nimaadiidar <youremail@example.com>
2026-06-30 12:51:54 -0700

    Add logout button

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import OrdersTable from "./OrdersTable";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-
+import Link from "next/link";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
@@ -79,9 +79,20 @@ const averageOrder =
     <main className="min-h-screen bg-black text-white p-10">
       <div className="max-w-7xl mx-auto">
 
-        <h1 className="text-5xl font-serif mb-10">
-          Orders
-        </h1>
+        <div className="flex justify-between items-center mb-10">
+
+  <h1 className="text-5xl font-serif">
+    Orders
+  </h1>
+
+  <Link
+    href="/api/admin/logout"
+    className="px-5 py-2 rounded-lg border border-neutral-700 hover:bg-neutral-800 transition"
+  >
+    Logout
+  </Link>
+
+</div>
 <div className="grid grid-cols-1 md:grid-cols-5 gap-6 mb-10">
   <div className="bg-neutral-900 rounded-xl p-6 border border-neutral-800">
     <p className="text-neutral-400">Total Sales</p>
