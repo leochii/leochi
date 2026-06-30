84a8b0850e093d5c4844f6eeb56ffd4501d6921e
nimaadiidar <youremail@example.com>
2026-06-30 13:33:39 -0700

    Hide navbar on admin pages

--- app/(shop)/checkout/page.tsx
+++ app/(shop)/checkout/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCart } from "../context/cartcontext"
+import { useCart } from "../../context/cartcontext"
 export default function CheckoutPage() {
   const { cart } = useCart();
 
